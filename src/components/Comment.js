5a25b3b9341914e596bdc891f0bfe6d1389202c4
Abiral Sthapit <abi.sthapit@gmail.com>
2018-05-03 01:11:50 +0545

    fix bug with disqus comment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,19 +2,20 @@ import React, { Component } from 'react'
 import Disqus from 'disqus-react';
 
 class Comment extends Component {
+    // handleNewComment(comment) {
+    //     window.console.info(`New comment posted with id ${comment.id} and message: ${comment.text}`);
+    // }
 
     render() {
-        const disqusShortname = this.props.post.slug;
+        const disqusShortname = "DevBlog"
         const disqusConfig = {
             url: this.props.post.url,
             identifier: this.props.post.slug,
             title: this.props.post.title,
-        };
+            // onNewComment: this.handleNewComment,
+        }
         return (
             <div className="blog_comments">
-                <Disqus.CommentCount shortname={disqusShortname} config={disqusConfig}>
-                    Comments
-                </Disqus.CommentCount>
                 <Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
             </div>
         )
